07fb23670633d8944e7a6c7bca816c9823ad50a0
Niki Saoulidou <sha1-d88223fadfe6b857b66f81a21dfe6b90f186505d@cern.ch>
2009-08-31 09:56:58 +0000

    Single Channel Monitoring Code

--- src/SealModule.cc
+++ src/SealModule.cc
@@ -2,6 +2,8 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #
 #include "RecoHcal/HcalPromptAnalysis/interface/HcalZS.h"
+#include "RecoHcal/HcalPromptAnalysis/interface/HcalChanMon.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(HcalZS);
+DEFINE_ANOTHER_FWK_MODULE(HcalChanMon);
