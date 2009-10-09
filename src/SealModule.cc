b969aa8d6000597c0b55b26092c1ed69941d3864
Francesco Santanastasio <sha1-89406cb43660773ab322fafa675b9f4e8b627eb0@cern.ch>
2009-10-09 09:44:51 +0000

    added AccessFEDinfo in SealModule

--- src/SealModule.cc
+++ src/SealModule.cc
@@ -5,9 +5,11 @@
 #include "RecoHcal/HcalPromptAnalysis/interface/HcalChanMon.h"
 #include "RecoHcal/HcalPromptAnalysis/interface/HcalPFGAnalysisTemplate.h"
 #include "RecoHcal/HcalPromptAnalysis/interface/JetTrigAnalyzer.h"
+#include "RecoHcal/HcalPromptAnalysis/interface/AccessFEDinfo.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(HcalZS);
 DEFINE_ANOTHER_FWK_MODULE(HcalChanMon);
 DEFINE_ANOTHER_FWK_MODULE(HcalPFGAnalysisTemplate);
 DEFINE_ANOTHER_FWK_MODULE(JetTrigAnalyzer);
+DEFINE_ANOTHER_FWK_MODULE(AccessFEDinfo);
